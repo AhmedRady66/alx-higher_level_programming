a35a020c4285aaaa833b59f9cf520d75a2b93b01
Ahmed Rady <rady20991@gmail.com>
2023-12-01 14:35:15 -0800

    Data Structures: Lists, Tuples

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 def replace_in_list(my_list, idx, element):
     for i in my_list:
         if idx < 0 or idx > len(my_list):
-            return my_list
+            return None
         elif idx == i:
             my_list[i] = element
             return my_list
