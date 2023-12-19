a64c97da2768c61628db042e17720082f5026134
Ahmed Rady <rady20991@gmail.com>
2023-12-19 11:55:22 -0800

    Python - Classes and Objects

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+"""Square class"""
+
 class Square:
     """Defines a square"""
+
     def __init__(self, size):
-        """Intialize the size data"""
+
+        """
+        Intialize the size data
+        """
         self.__size = size
