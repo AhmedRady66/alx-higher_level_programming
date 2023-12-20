257b332d3874901e0242e046897fec84257e3196
Ahmed Rady <rady20991@gmail.com>
2023-12-19 16:18:30 -0800

    Python - Classes and Objects

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""class of square"""
+class Square:
+    """define the class"""
+    def __init__(self, size=0):
+        """attributes
+        size: size of square
+        """
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        
+    def area(self):
+       """Return the area of square"""
+       return self.__size**2
