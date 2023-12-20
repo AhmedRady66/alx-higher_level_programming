39cf04d3d8ba9b7f2528fecb534d53ddcc76a420
Ahmed Rady <rady20991@gmail.com>
2023-12-19 16:42:50 -0800

    Python - Classes and Objects

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""class of square"""
+class Square:
+    """define the class"""
+    def __init__(self, size=0):
+        """attributes
+        size: size of square
+        """
+        self.size = size
+        
+    def area(self):
+        """Return the area of square"""
+        return self.__size**2
+    @property
+    def size(self):
+        """return size as getter method"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """set size using setter method"""
+        self.__size = value
+        if type(value) != int:
+             raise TypeError("size must be an integer")
+        if value < 0:
+             raise ValueError("size must be >= 0")
+
+    def my_print(self):
+        """
+        print # specific for input size
+        """
+        if self.__size > 0:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
+        elif self.__size == 0:
+            print()
