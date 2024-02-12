4187d3e56860a942f39cb9f06dd51db6ad8dca46
Ahmed Rady <rady20991@gmail.com>
2024-02-12 13:09:54 -0800

    Real definition of a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,40 +4,19 @@
 
 class Rectangle:
     """Define Rectangle class"""
-
     def __init__(self, width=0, height=0):
-        """Initialize args of the class
-
-        Args:
-            height: input height of rectangle
-            width: input width of rectangle
-        """
+        """Initialize the rectangle"""
         self.__height = height
         self.__width = width
 
     @property
     def width(self):
-        """width getter.
-
-        Returns:
-            __width (int): horizontal dimension of rectangle
-
-        """
+        """width getter for the attribute"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Args:
-            value (int): horizontal dimension of rectangle
-
-        Attributes:
-            __width (int): horizontal dimension of rectangle
-
-        Raises:
-            TypeError: If value is not an int.
-            ValueError: If value is less than 0.
-
-        """
+        """width setter for the attribute"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if self.__width < 0:
@@ -46,27 +25,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """height getter.
-
-        Returns:
-            __height (int): vertical dimension of rectangle
-
-        """
+        """height getter for the attribute"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Args:
-            value (int): vertical dimension of rectangle
-
-        Attributes:
-            __height (int): vertical dimension of rectangle
-
-        Raises:
-            TypeError: If value is not an int.
-            ValueError: If value is less than 0.
-
-        """
+        """height setter for the attribute"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if self.__height < 0:
