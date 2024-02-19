7e4310ecc3057c1cd0e39fa53683d900cbff97d1
Ahmed Rady <rady20991@gmail.com>
2024-02-19 10:43:49 -0800

    Inheritance

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Module for lookup"""
+
+def lookup(obj):
+    """returns the list of available attributes and methods
+    Args:
+        obj: object input
+    Return:
+        list of attributes
+    """
+    return dir(obj)
