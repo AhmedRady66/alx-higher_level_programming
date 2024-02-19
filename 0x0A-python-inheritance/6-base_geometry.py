bbce9cd0b6a86ee506b95d94d445af07e9fb0e6b
Ahmed Rady <rady20991@gmail.com>
2024-02-19 11:37:25 -0800

    Improve Geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""BaseGeometry class"""
+
+
+class BaseGeometry():
+    """Define BaseGeometry class"""
+    def area(self):
+        """Method to find area"""
+        raise Exception("area() is not implemented")
