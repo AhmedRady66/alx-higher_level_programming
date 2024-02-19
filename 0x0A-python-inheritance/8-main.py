5df5d56c1b6b9d1f38552d8a7d73d5cc17e497fe
Ahmed Rady <rady20991@gmail.com>
2024-02-19 12:03:16 -0800

    Rectangle

--- 0x0A-python-inheritance/8-main.py
+++ 0x0A-python-inheritance/8-main.py
@@ -14,4 +14,4 @@ except Exception as e:
 try:
     r2 = Rectangle(4, True)
 except Exception as e:
-    print("[{}] {}".format(e.__class__.__name__, ))
+    print("[{}] {}".format(e.__class__.__name__, e))
