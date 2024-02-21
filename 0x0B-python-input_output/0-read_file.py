5fee9982e41365888df515aa535fcb58eabf627e
Ahmed Rady <rady20991@gmail.com>
2024-02-21 14:14:56 -0800

    Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+"""read_file function"""
 
 
 def read_file(filename=""):
+    """read filename input"""
     with open(filename, mode="r", encoding="UTF-8") as f:
         print(f.read(), end="")
