ddb5f5ff68ce6394bfc43ac101d17a45f39fa517
Ahmed Rady <rady20991@gmail.com>
2024-02-26 14:56:06 -0800

    Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Base Class"""
+
+
+class Base:
+    """Define Base class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize"""
+        if id != None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
