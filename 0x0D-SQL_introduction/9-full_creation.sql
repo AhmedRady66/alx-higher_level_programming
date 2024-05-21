500cec6f0a730584a7d77b98e981dccc00b2e78d
Ahmed Rady <rady20991@gmail.com>
2024-05-22 02:32:06 +0300

    Full creation

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,6 @@
+-- script that creates a table second_table in the database hbtn_0c_0
+CREATE TABLE IF NOT EXISTS second_table(Id INT, Name VARCHAR(256), Score INT)
+INSERT INTO second_table(Id, Name, Score) VALUES(1, 'John', 10)
+INSERT INTO second_table(Id, Name, Score) VALUES(2, 'Alex', 3)
+INSERT INTO second_table(Id, Name, Score) VALUES(3, 'Bob', 14)
+INSERT INTO second_table(Id, Name, Score) VALUES(4, 'George', 8)
