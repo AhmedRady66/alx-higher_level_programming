53b788a3b899ab120c1e8bb2e83a9ae01b4dd230
Ahmed Rady <rady20991@gmail.com>
2024-05-22 02:56:13 +0300

    Say my name

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+-- script that lists all records of the table second_table
+SELECT score, name FROM second_table ORDER BY score DESC;
+/*Don’t list rows without a name value
+Results should display the score and the name (in this order)
+Records should be listed by descending score*/
