e0c3498a61de01684301a03baa2ef0b2639786b2
Ahmed Rady <rady20991@gmail.com>
2024-05-22 02:36:15 +0300

    List by best

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 --  script that lists all records of the table second_table of the database
-SELECT Score, Name FROM second_table ORDER BY Score ASC;
+SELECT score, name FROM second_table ORDER BY Score DESC;
