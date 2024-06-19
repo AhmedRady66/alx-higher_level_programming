3538c133d83993c5ad43d859f8a89821cb9050fd
Ahmed Rady <rady20991@gmail.com>
2024-06-20 00:23:14 +0300

    Rectangle #2

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  width;
+  height;
+
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+      this.width = undefined;
+      this.height = undefined;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+};
