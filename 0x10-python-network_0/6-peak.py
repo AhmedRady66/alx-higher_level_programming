8bb2463588f6048bf2f4fd8bfc6543f364453b6b
Ahmed Rady <rady20991@gmail.com>
2024-07-04 23:31:09 +0300

    Find a peak

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Find a peak in a list of unsorted integers"""
+
+
+def find_peak(list_of_integers):
+    result = 0
+    if len(list_of_integers) == 0:
+        return None
+    for peak in range(len(list_of_integers)):
+        if peak == 0 and list_of_integers[peak] >= list_of_integers[peak+1]:
+            result = list_of_integers[peak]
+        elif peak == list_of_integers[-1] and \
+                list_of_integers[peak] >= list_of_integers[peak-1]:
+            result = list_of_integers[peak]
+        elif list_of_integers[peak] >= list_of_integers[peak-1] \
+                and list_of_integers[peak] >= list_of_integers[peak+1]:
+            result = list_of_integers[peak]
+    return result
