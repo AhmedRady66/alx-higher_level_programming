4af40e0dd84d232d1d410985bf958528b900d312
Ahmed Rady <rady20991@gmail.com>
2024-07-06 01:48:11 +0300

    Error code #1

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -5,9 +5,7 @@ import sys
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-
-    r = requests.get(url)
+    r = requests.get(sys.argv[1])
     if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
     else:
