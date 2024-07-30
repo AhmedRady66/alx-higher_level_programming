eb910bd036b6ebf641fbca7886748c67b9abb546
Ahmed Rady <rady20991@gmail.com>
2024-07-31 00:27:03 +0300

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,10 +2,8 @@
 
 const request = require('request');
 
-// The URL of the Star Wars API for films
 const url = process.argv[2];
 
-// The character ID for Wedge Antilles
 const characterId = 18;
 const characterUrl = `https://swapi-api.alx-tools.com/api/people/${characterId}/`;
 
